88b76d545c6127197702b8a4af1f0d6b86d7ec4a
Mladena <Mladena@10.0.2.15>
2012-10-21 23:59:58 +0300

    Add DataAccessException.

---
+++ src/dao/DataAccessException.java
@@ -0,0 +1,32 @@
+package dao;
+
+public class DataAccessException extends Exception {
+
+	public DataAccessException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
