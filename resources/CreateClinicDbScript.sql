3a4c12405f2910de37f90f03ce033344b61ded21
Mladena <Mladena@10.0.2.15>
2012-10-27 15:39:12 +0300

    The DB creation script changes.

--- resources/CreateClinicDbScript.sql
+++ resources/CreateClinicDbScript.sql
@@ -1,3 +1,16 @@
+drop table "ADMIN"."DOCTORS" cascade constraints PURGE;
+drop table "ADMIN"."DOCTORS_SPECIALTIES" cascade constraints PURGE;
+drop table "ADMIN"."PATIENTS" cascade constraints PURGE;
+drop table "ADMIN"."PERSONS" cascade constraints PURGE;
+drop table "ADMIN"."SPECIALTIES" cascade constraints PURGE;
+drop table "ADMIN"."TITLES" cascade constraints PURGE;
+drop table "ADMIN"."VISITS" cascade constraints PURGE;
+
+drop sequence "ADMIN"."PERSONS_SEQ";
+drop sequence "ADMIN"."SPECIALTIES_SEQ";
+drop sequence "ADMIN"."TITLES_SEQ";
+drop sequence "ADMIN"."VISITS_SEQ";
+
 create table PERSONS (
   "ID" NUMBER(10,0) not null,
   FIRST_NAME NVARCHAR2(50) not null,
@@ -10,18 +23,12 @@ create table PATIENTS (
   "ID" NUMBER(10,0) not null,
   AGE NUMBER(10,0) not null,
   EGN NVARCHAR2(25) not null,
-  PERSON_ID NUMBER(10,0) not null,
   constraint PATIENTS_PK primary key ("ID") );
-
-create sequence PATIENTS_SEQ;
 
 create table DOCTORS (
   "ID" NUMBER(10,0) not null,
   TITLE_ID NUMBER(10,0) not null,
-  PERSON_ID NUMBER(10,0) not null,
   constraint DOCTORS_PK primary key ("ID") );
-
-create sequence DOCTORS_SEQ;
 
 create table TITLES (
   "ID" NUMBER(10,0) not null,
@@ -54,11 +61,11 @@ create table DOCTORS_SPECIALTIES(
   constraint DOCTORS_SPECIALTIES_PK primary key (DOCTOR_ID, SPECIALTY_ID) );
 
 alter table PATIENTS
-  add constraint PATIENTS_PERSONS_FK foreign key (PERSON_ID)
+  add constraint PATIENTS_PERSONS_FK foreign key ("ID")
   references PERSONS ("ID");
 
 alter table DOCTORS
-  add constraint DOCTORS_PERSONS_FK foreign key (PERSON_ID)
+  add constraint DOCTORS_PERSONS_FK foreign key ("ID")
   references PERSONS ("ID"); 
 
 alter table DOCTORS
@@ -102,16 +109,16 @@ values (9, 'Dimityr', 'Stoqnov');
 insert into PERSONS ("ID", FIRST_NAME, LAST_NAME)
 values (10, 'Vanq', 'Genadieva');
 
-insert into PATIENTS ("ID", AGE, EGN, PERSON_ID)
-values (1,27,8505081111,1);
-insert into PATIENTS ("ID", AGE, EGN, PERSON_ID)
-values (2,27,8502082222,2);
-insert into PATIENTS ("ID", AGE, EGN, PERSON_ID)
-values (3,30,8205083333,3);
-insert into PATIENTS ("ID", AGE, EGN, PERSON_ID)
-values (4,62,5003244444,4);
-insert into PATIENTS ("ID", AGE, EGN, PERSON_ID)
-values (5,31,8812045555,5);
+insert into PATIENTS ("ID", AGE, EGN)
+values (1,27,8505081111);
+insert into PATIENTS ("ID", AGE, EGN)
+values (2,27,8502082222);
+insert into PATIENTS ("ID", AGE, EGN)
+values (3,30,8205083333);
+insert into PATIENTS ("ID", AGE, EGN)
+values (4,62,5003244444);
+insert into PATIENTS ("ID", AGE, EGN)
+values (5,31,8812045555);
 
 insert into TITLES("ID", "NAME")
 values (1,'PROFFESSOR');
@@ -120,16 +127,16 @@ values (2,'DOCTOR');
 insert into TITLES("ID", "NAME")
 values (3,'DOCENT');
 
-insert into DOCTORS ("ID", TITLE_ID, PERSON_ID)
-values (1,1,6);
-insert into DOCTORS ("ID", TITLE_ID, PERSON_ID)
-values (2,2,7);
-insert into DOCTORS ("ID", TITLE_ID, PERSON_ID)
-values (3,3,8);
-insert into DOCTORS ("ID", TITLE_ID, PERSON_ID)
-values (4,1,9);
-insert into DOCTORS ("ID", TITLE_ID, PERSON_ID)
-values (5,2,10);
+insert into DOCTORS ("ID", TITLE_ID)
+values (6,1);
+insert into DOCTORS ("ID", TITLE_ID)
+values (7,2);
+insert into DOCTORS ("ID", TITLE_ID)
+values (8,3);
+insert into DOCTORS ("ID", TITLE_ID)
+values (9,1);
+insert into DOCTORS ("ID", TITLE_ID)
+values (10,2);
 
 insert into SPECIALTIES("ID", "NAME")
 values (1,'IMMUNOLOGY');
@@ -149,37 +156,37 @@ insert into SPECIALTIES("ID", "NAME")
 values (8,'EARS-NOSE-THROAT');
 
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (5,1);
+values (6,1);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (4,2);
+values (8,2);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,1);
+values (7,1);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,2);
+values (7,2);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,3);
+values (7,3);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,4);
+values (7,4);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,5);
+values (7,5);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,6);
+values (7,6);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,7);
+values (7,7);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (2,8);
+values (7,8);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (1,5);
+values (9,5);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (3,7);
+values (9,7);
 insert into DOCTORS_SPECIALTIES(DOCTOR_ID, SPECIALTY_ID)
-values (4,8);
+values (10,8);
 
 insert into VISITS("ID", "DATE", PRICE, DIAGNOSE, PATIENT_ID, DOCTOR_ID)
-values (1, '01-AUG-2012', 20.55, 'incurable stupidity', 1, 2);
+values (1, '01-AUG-2012', 20.55, 'incurable stupidity', 1, 6);
 insert into VISITS("ID", "DATE", PRICE, DIAGNOSE, PATIENT_ID, DOCTOR_ID)
-values (2, '23-SEP-2012', 12.60, 'lazyness', 2, 2);
+values (2, '23-SEP-2012', 12.60, 'lazyness', 2, 7);
 insert into VISITS("ID", "DATE", PRICE, DIAGNOSE, PATIENT_ID, DOCTOR_ID)
-values (3, '10-OCT-2012', 4.16, 'needs some happy pills', 1, 2);
+values (3, '10-OCT-2012', 4.16, 'needs some happy pills', 1, 8);
 
 commit;
